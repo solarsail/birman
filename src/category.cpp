be0a8072f2b6975971d857c9f3ef1f29b9ccf049
rtty122333 <wangbo7799@gmail.com>
2014-08-22 00:20:51 +0800

    修改setTypes函数
    参数换为string类型

--- src/category.cpp
+++ src/category.cpp
@@ -2,16 +2,20 @@
 
 bool CategorySet::hasType(Category::Type type)
 {
-    return _typeSet.test(type);
+	return _typeSet.test(type);
 }
 
 void CategorySet::addType(Category::Type type)
 {
-    _typeSet.set(type);
+	_typeSet.set(type);
 }
 
 void CategorySet::removeType(Category::Type type)
 {
-    _typeSet.reset(type);
+	_typeSet.reset(type);
 }
-
+void CategorySet::setTypes(std::string mask)
+{
+	std::bitset<Category::TypeCount> tmp(mask);
+	_typeSet = tmp;
+}
